b69a6a4437ffbed336aad7ef439a053e78ddb0a6
Can Kutlu Kinay <can@unumotors.com>
2017-11-28 16:17:58 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+
+const fs = require('fs');
+const ConfigParser = require('cordova-common').ConfigParser;
+
+const version = process.env.npm_package_version
+    || process.argv[2]
+    || JSON.parse(fs.readFileSync('./package.json', { encoding: 'UTF-8' })).version;
+
+const config = new ConfigParser('./config.xml');
+
+config.setVersion(version);
+config.write();
